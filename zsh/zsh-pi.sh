cfb8cdec6a921d0233939c5235ec5b74f8fddce8
tangh18 <tangh18.thu@gmail.com>
2020-12-26 12:44:36 +0800

    zsh for pi

---
+++ zsh/zsh-pi.sh
@@ -0,0 +1,9 @@
+apt install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+cd ~/.oh-my-zsh/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone git://github.com/zsh-users/zsh-autosuggestions
+cd ~/script
+rm ~/.zshrc
+cp zsh/conf/zsh-pi.conf ~/.zshrc
+source ~/.zshrc
